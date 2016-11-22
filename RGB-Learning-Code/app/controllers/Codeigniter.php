eddd4f6e4d978c5692576713758a8e1a970467dc
florian dahlitz <f2dahlitz@freenet.de>
2016-11-22 21:17:13 +0100

    update files

--- RGB-Learning-Code/app/controllers/Codeigniter.php
+++ RGB-Learning-Code/app/controllers/Codeigniter.php
@@ -4,21 +4,39 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Codeigniter extends MY_Controller 
 {
 
-	/**
-	 * type: controller
-	 * handles Codeigniter page
-	 *
-	 * @author Florian Dahlitz
-	 */
+    /**
+     * type: controller
+     * handles Codeigniter page
+     *
+     * @author Florian Dahlitz
+     */
 
-	public function __construct()
-	{
-		parent::__construct();
-	}
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	public function index()
-	{
-		$this->data['subview'] = 'codeigniter';
-		$this->load->view('layout', $this->data);
-	}
+    public function index()
+    {
+        $this->data['subview'] = 'codeigniter';
+        $this->load->view('layout', $this->data);
+    }
+    
+    public function download()
+    {
+        $this->data['subview'] = 'codeigniter/download';
+        $this->load->view('layout', $this->data);
+    }
+    
+    public function adjust_xampp()
+    {
+        $this->data['subview'] = 'codeigniter/adjust_xampp';
+        $this->load->view('layout', $this->data);
+    }
+    
+    public function basic_settings()
+    {
+        $this->data['subview'] = 'codeigniter/basic_settings';
+        $this->load->view('layout', $this->data);
+    }
 }
---
+++ RGB-Learning-Code/app/views/codeigniter.php
@@ -0,0 +1,6 @@
+<h2>Codeigniter (CI)</h2>
+<ul>
+    <li><?php echo anchor(base_url('codeigniter/download'), 'Download CI'); ?></li>
+    <li><?php echo anchor(base_url('codeigniter/adjust_xampp'), 'localhost anpassen (XAMPP)'); ?></li>
+    <li><?php echo anchor(base_url('codeigniter/basic_settings'), 'Grundeinstellung von CI'); ?></li>
+</ul>
